76862436a606561c6b79487e3ea8f043c0de0735
Vince Carey <stvjc@channing.harvard.edu>
2023-02-28 09:58:16 -0500

    Update basilisk.R

--- R/basilisk.R
+++ R/basilisk.R
@@ -7,6 +7,6 @@ bsklenv <- basilisk::BasiliskEnvironment(envname="bsklenv",
     pkgname="scviR",
     packages=c("numpy==1.23.1", "pandas==1.4.4"),
       pip=c("scvi-tools==0.20.0", "scanpy==1.9.1", # "scikit-misc==0.1.4",
-       "matplotlib==3.6.3", "jax==0.4.1"))
+       "matplotlib==3.6.3", "jax==0.4.4"))
 
 # dropping scikit-misc on 18 Feb because it can't be used on M1 mac
