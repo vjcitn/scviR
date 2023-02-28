0848a81927ef357659ae3afb677491577703dfb1
vjcitn <stvjc@channing.harvard.edu>
2023-02-28 01:26:45 -0500

    dealing with the 'not in usage' error found in actions

--- R/data.R
+++ R/data.R
@@ -1,9 +1,11 @@
 #' mRNA-based cluster labels for 7472 cells in OSCA chapter 12 analysis
 #' @docType data
+#' @usage data(clusters.rna)
 #' @format factor
 "clusters.rna"
 
 #' ADT-based cluster labels for 7472 cells in OSCA chapter 12 analysis
 #' @docType data
+#' @usage data(clusters.adt)
 #' @format factor
 "clusters.adt"
